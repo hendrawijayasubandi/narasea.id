f1a7341a1449fc8ab166293fe596ab2ab567e8ea
hendrawijayasubandi <hendrawijayasubandi@gmail.com>
2025-04-29 08:33:16 +0700

    add hover footer for submenu link

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,12 +1,17 @@
+<style>
+    a:hover {
+    color: #017C85;
+}
+</style>
 <div class="grid grid-cols-1 gap-8 text-white sm:grid-cols-3 font-calimate">
     <!-- See Our Ocean -->
     <div>
         <h3 class="text-lg font-semibold">See Our Ocean</h3>
         <ul class="mt-4 space-y-2 text-sm text-gray-300 font-ttNorms">
-            <li><a href="{{ url('/#tides-of-impact') }}" class="hover:text-white">Tides of Impact</a></li>
-            <li><a href="{{ url('/#voice-from-the-ocean') }}" class="hover:text-white">Voice From The Ocean</a></li>
-            <li><a href="{{ url('/#protect-educate-empower') }}" class="hover:text-white">Protect, Educate, Empower</a></li>
-            <li><a href="{{ url('/#social-current') }}" class="hover:text-white">Social Current</a></li>
+            <li><a href="{{ url('/#tides-of-impact') }}" class="hover:text-teal-blue transition duration-300 ease-in-out">Tides of Impact</a></li>
+            <li><a href="{{ url('/#voice-from-the-ocean') }}" class="hover:text-teal-blue transition duration-300 ease-in-out">Voice From The Ocean</a></li>
+            <li><a href="{{ url('/#protect-educate-empower') }}" class="hover:text-teal-blue transition duration-300 ease-in-out">Protect, Educate, Empower</a></li>
+            <li><a href="{{ url('/#social-current') }}" class="hover:text-teal-blue transition duration-300 ease-in-out">Social Current</a></li>
         </ul>
     </div>
 
@@ -14,11 +19,11 @@
     <div>
         <h3 class="text-lg font-semibold">Raise the Tide</h3>
         <ul class="mt-4 space-y-2 text-sm text-gray-300 font-ttNorms">
-            <li><a href="{{ url('/share#srt-educate') }}" class="hover:text-white">Educate</a></li>
-            <li><a href="{{ url('/share#support-marine-conservation') }}" class="hover:text-white">Support Marine Conservation</a></li>
-            <li><a href="{{ url('/share#learning-action') }}" class="hover:text-white">Learning & Action</a></li>
-            <li><a href="{{ url('/share#empower-communitites') }}" class="hover:text-white">Empower Communities</a></li>
-            <li><a href="{{ url('/share#advocate-amplify')}}" class="hover:text-white">Advocate & Amplify</a></li>
+            <li><a href="{{ url('/share#srt-educate') }}" class="hover:text-teal-blue transition duration-300 ease-in-out">Educate</a></li>
+            <li><a href="{{ url('/share#support-marine-conservation') }}" class="hover:text-teal-blue transition duration-300 ease-in-out">Support Marine Conservation</a></li>
+            <li><a href="{{ url('/share#learning-action') }}" class="hover:text-teal-blue transition duration-300 ease-in-out">Learning & Action</a></li>
+            <li><a href="{{ url('/share#empower-communitites') }}" class="hover:text-teal-blue transition duration-300 ease-in-out">Empower Communities</a></li>
+            <li><a href="{{ url('/share#advocate-amplify')}}" class="hover:text-teal-blue transition duration-300 ease-in-out">Advocate & Amplify</a></li>
         </ul>
     </div>
 
@@ -26,11 +31,11 @@
     <div>
         <h3 class="text-lg font-semibold">Discover Narasea</h3>
         <ul class="mt-4 space-y-2 text-sm text-gray-300 font-ttNorms">
-            <li><a href="{{ url('/sea#who-we-are') }}" class="hover:text-white">Who We Are</a></li>
-            <li><a href="{{ url('/sea#our-journey') }}" class="hover:text-white">Our Journey</a></li>
-            <li><a href="{{ url('/sea#our-vision-mission-approach') }}" class="hover:text-white">Our Vision, Mission, & Approach</a></li>
-            <li><a href="{{ url('/sea#the-values-we-live-by')}}" class="hover:text-white">The Values We Live By</a></li>
-            <li><a href="{{ url('/sea#dc-advocate-amplify') }}" class="hover:text-white">Advocate & Amplify</a></li>
+            <li><a href="{{ url('/sea#who-we-are') }}" class="hover:text-teal-blue transition duration-300 ease-in-out">Who We Are</a></li>
+            <li><a href="{{ url('/sea#our-journey') }}" class="hover:text-teal-blue transition duration-300 ease-in-out">Our Journey</a></li>
+            <li><a href="{{ url('/sea#our-vision-mission-approach') }}" class="hover:text-teal-blue transition duration-300 ease-in-out">Our Vision, Mission, & Approach</a></li>
+            <li><a href="{{ url('/sea#the-values-we-live-by')}}" class="hover:text-teal-blue transition duration-300 ease-in-out">The Values We Live By</a></li>
+            <li><a href="{{ url('/sea#dc-advocate-amplify') }}" class="hover:text-teal-blue transition duration-300 ease-in-out">Advocate & Amplify</a></li>
         </ul>
     </div>
 </div>
